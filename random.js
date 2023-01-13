93df3d7a02e423c1f1f967bd738cd1285d495182
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-12 22:54:07 -1200

    feat: added the quotes object

---
+++ random.js
@@ -0,0 +1,12 @@
+// variables
+
+let btn = document.querySelector("#new-quote");
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".person");
+
+const quotes = [{
+        quote: "The best way to find yourself is to lose yourself in the service to others.",
+        person: "Mahatma Gandhi"
+}, {
+
+}]
