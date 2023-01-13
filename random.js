ceee3bc195e3334fabab3d0bdee42ba4adc4c493
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-12 23:07:43 -1200

    feat: added more quotes to the object array

--- random.js
+++ random.js
@@ -7,6 +7,31 @@ let person = document.querySelector(".person");
 const quotes = [{
         quote: "The best way to find yourself is to lose yourself in the service to others.",
         person: "Mahatma Gandhi"
-}, {
-
-}]
+    }, {        
+        quote: "If you want to live a happy life, tie it to a goal, not to people or things",
+        person: "Albert Einstein"
+    }, {
+        quote: "At his best, man is the noblest of all animals; seperated from the law and justice he is the worst",
+        person: "Aristotle"
+    }, {
+        quote: "Your time is limited, so don't waste it living someone else's life.",
+        person: "Steve Jobs"        
+    }, {
+        quote: "Tell me and I forget. Teach me and I remember, Invlove me and I learn.",
+        person: "Benjamin Franklin"
+    }, {
+        quote: "If you look at what you have in life, you'll always have more,If you look at what you don't have in life, you'll never have enough.",
+        person: "Oprah Winfrey"
+    }, {
+        quote: "It does not matter how slowly you go as long as you do not stop.",
+        person: "Confucius"
+    }, {
+        quote: "Our lives begin to end the day we become silent about things that matter.",
+        person: "Martin Luther King, Jr."
+    }, {
+        quote: "Remember taht not getting what you want is sometimes a wonderful stroke of luck",
+        person: "Dalia Lama"
+    }, {
+        quote: "The journey of a thousand miles, begin with one step.",
+        person: "Lao Tzu"
+    }]
