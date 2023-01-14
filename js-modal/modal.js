3874b9f6ddad7fcbe1a10104a8ff813abf38ee55
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-13 14:18:56 -1200

    feat: added behavior to button event and added styles to modal

---
+++ js-modal/modal.js
@@ -0,0 +1,20 @@
+//Variables
+
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
+
+//Event listener
+
+openBtn.addEventListener("click", function() {
+    modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function() {
+    closeBtn.style.display = "none";
+});
+
+window.addEventListener("click", function(e) {
+    if(e.target === modalContainer)
+    modalContainer.style.display = "none";
+})
